71176e3c8d1e0cb446608654ba9cd141deda8ecc
Vit Bokisch <vit@bokisch.cz>
2021-02-01 16:01:12 +0100

    rollup: add missing typings

---
+++ packages/rollup/typings/index.d.ts
@@ -0,0 +1,20 @@
+/// <reference types="node" />
+export {}
+
+declare global {
+  const __BROWSER__: true
+  const __WEB__: true
+  const __NATIVE__: true
+  const __SERVER__: true
+  const __CLIENT__: true
+}
+
+declare global {
+  interface Window {
+    __BROWSER__: true
+    __WEB__: true
+    __NATIVE__: true
+    __SERVER__: true
+    __CLIENT__: true
+  }
+}
