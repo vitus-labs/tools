bfcd612df2604156cc128fe0fa6783ea89bfd70d
Vit Bokisch <vit@bokisch.cz>
2021-02-04 12:52:21 +0100

    storybook: fix

--- packages/storybook/src/storybook/preview.ts
+++ packages/storybook/src/storybook/preview.ts
@@ -2,7 +2,7 @@
 /* eslint-disable no-undef */
 import { MINIMAL_VIEWPORTS, INITIAL_VIEWPORTS } from '@storybook/addon-viewport'
 import { withKnobs, text, boolean, number } from '@storybook/addon-knobs'
-import { themeDecorator } from '~/decorators'
+import { themeDecorator } from '../decorators'
 
 declare global {
   const __VITUS_LABS_STORIES__: Record<string, unknown>
@@ -10,14 +10,14 @@ declare global {
 
 declare global {
   interface Window {
-  __VITUS_LABS_STORIES__: Record<string, unknown>
-  __STORY__: {
-    text: typeof text
-    boolean: typeof boolean
-    number: typeof number
-    withKnobs: typeof withKnobs
+    __VITUS_LABS_STORIES__: Record<string, unknown>
+    __STORY__: {
+      text: typeof text
+      boolean: typeof boolean
+      number: typeof number
+      withKnobs: typeof withKnobs
+    }
   }
-}
 }
 
 // define configuration globally in window so it can be accessible from anywhere in the browser
