14a0481610fd93508e015fb5d514da117d1c1ab2
Vit Bokisch <vit@bokisch.cz>
2021-04-15 11:52:04 +0200

    storybook default init configuration for styling (#6)

--- packages/storybook/src/storybook/preview.ts
+++ packages/storybook/src/storybook/preview.ts
@@ -1,6 +1,29 @@
 /* eslint-disable no-underscore-dangle */
 /* eslint-disable no-undef */
+import { init } from '@vitus-labs/core'
 import * as d from '../decorators'
+
+if (__VITUS_LABS_STORIES__.styles === 'styled-components') {
+  const styled = require('styled-components')
+
+  init({
+    styled: styled.default,
+    css: styled.css,
+    context: styled.ThemeProvider,
+  })
+}
+
+if (__VITUS_LABS_STORIES__.styles === 'emotion') {
+  const styled = require('@emotion/styled')
+  const helper = require('@emotion/react')
+  const { ThemeProvider } = require('emotion-theming')
+
+  init({
+    styled: styled.default,
+    css: helper.css,
+    context: ThemeProvider,
+  })
+}
 
 declare global {
   const __VITUS_LABS_STORIES__: Record<string, unknown>
@@ -20,7 +43,9 @@ export const globalTypes = __VITUS_LABS_STORIES__.globals
 export const decorators = Object.entries(
   __VITUS_LABS_STORIES__.decorators
 ).reduce((acc, [key, value]) => {
-  if (value) acc.push(d[key](value))
+  if (value) {
+    acc.push(d[key](value))
+  }
   return acc
 }, [])
 
