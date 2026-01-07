473fc499a713f5f714d3cf26b8a490de40ed024e
Vít Bokisch <vit@bokisch.cz>
2026-01-07 13:00:55 +0100

    fix types

--- packages/types/src/index.d.ts
+++ packages/types/src/index.d.ts
@@ -15,42 +15,38 @@ export type StrictOmit<T, K extends keyof T> = Omit<T, K>
  */
 export type ExtractProps<TComponentOrTProps> =
   TComponentOrTProps extends ComponentType<infer TProps>
-    ? TProps
-    : TComponentOrTProps
+  ? TProps
+  : TComponentOrTProps
 
 // --------------------------------------------------------
 // MERGE TYPES
 // --------------------------------------------------------
-
 /**
  * @private
  */
-export type Id<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
+type Id<T> = T extends infer U ? { [K in keyof U]: U[K] } : never
 
 /**
  * @private
+ * Only filters out properties that are exactly never, null, or undefined
  */
-export type ExtractNullableKeys<T> = {
-  [P in keyof T as T[P] extends null | never | undefined ? never : P]: T[P]
+type ExtractNullableKeys<T> = {
+  [P in keyof T as[T[P]] extends [never]
+  ? never
+  : [T[P]] extends [null | undefined]
+  ? never
+  : P]: T[P]
 }
 
 /**
  * @private
  */
-export type SpreadTwo<L, R> = Id<
-  Pick<L, Exclude<keyof L, keyof R>> & R
-  // Pick<R, Exclude<keyof R, OptionalPropertyNames<R>>> &
-  // Pick<R, Exclude<OptionalPropertyNames<R>, keyof L>> &
-  // SpreadProperties<L, R, OptionalPropertyNames<R> & keyof L>
->
+type SpreadTwo<L, R> = Id<Pick<L, Exclude<keyof L, keyof R>> & R>
 
 /**
  * @private
  */
-export type Spread<A extends readonly [...any]> = A extends [
-  infer L,
-  ...infer R
-]
+type Spread<A extends readonly [...any]> = A extends [infer L, ...infer R]
   ? SpreadTwo<L, Spread<R>>
   : unknown
 
